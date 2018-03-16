604cf944ccefad48975c4ec632aa9ef3a31aadf9
Juan Esteban Camargo <juanescacha02@hotmail.com>
2018-03-15 23:08:02 -0500

    objetos despues de hacer el compilado y correr

---
+++ cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/Estudiante.cpp" "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/Estudiante.cpp.obj"
+  "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/Grupo.cpp" "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/Grupo.cpp.obj"
+  "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/main.cpp" "C:/Users/Juanescacha/Documents/GitHub/Practica-Arreglo-Objetos/cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Ejemplo_de_Clase_2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejemplo_de_Clase_2.dir/Estudiante.cpp.obj"
+  "CMakeFiles/Ejemplo_de_Clase_2.dir/Grupo.cpp.obj"
+  "CMakeFiles/Ejemplo_de_Clase_2.dir/main.cpp.obj"
+  "Ejemplo_de_Clase_2.pdb"
+  "Ejemplo_de_Clase_2.exe"
+  "Ejemplo_de_Clase_2.exe.manifest"
+  "libEjemplo_de_Clase_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejemplo_de_Clase_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
